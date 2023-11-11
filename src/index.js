fc4ca81ace68dbc9a7fe65742bc8b4b9a6c7ba28
tetianattl <tetyana.ttl@gmail.com>
2023-11-11 14:26:00 +0200

    added basic search engine (no API)

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city")
+    cityElement.innerHTML = searchInput.value;
+    console.log(cityElement);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
