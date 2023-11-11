67a5726ccb97b2067c493472a45780c593b55410
tetianattl <tetyana.ttl@gmail.com>
2023-11-11 17:45:19 +0200

    added the search functionnality using the Api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  console.log(response.data);
+}
+
+function searchCity(city) {
+  let apiKey = "eb9542c65e739e0fb25ade97c749e2aa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city")
-    cityElement.innerHTML = searchInput.value;
-    console.log(cityElement);
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Kyiv");
